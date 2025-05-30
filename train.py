d930eca6f7f7144b1aa84828031aa6331cd86894
Cont Thai <61564643+zerokhong1@users.noreply.github.com>
2025-05-30 21:22:49 +0700

    Update train.py to tiny

--- train.py
+++ train.py
@@ -58,7 +58,7 @@ def main():
     # model1 = Unet.Unet(input_channel = 3)
     model1 = Swin_unet.SwinUnet() 
     # Swin_unet.load_pretrained_encoder(model1)
-    Swin_unet.load_pretrained_encoder(model1, "swinv2_tiny_patch4_window16_256.pth")
+    Swin_unet.load_pretrained_encoder(model1, "swinv2_tiny_patch4_window8_256.pth)")
     optimizer1 = optimizer.optimizer(model = model1)
     trainer = Trainer(model = model1, optimizer = optimizer1)
     trainLoader, validLoader, testLoader = get_dataloaders(args.augment)
